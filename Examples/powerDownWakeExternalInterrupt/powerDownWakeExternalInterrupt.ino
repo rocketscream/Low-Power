fe7ada44c57a1474925d6641811435a39e55bb8d
rufuswilson <marsou75@gmail.com>
2017-11-16 18:07:48 +0100

    Updated to v1.7

--- Examples/powerDownWakeExternalInterrupt/powerDownWakeExternalInterrupt.ino
+++ Examples/powerDownWakeExternalInterrupt/powerDownWakeExternalInterrupt.ino
@@ -13,21 +13,40 @@ void setup()
 {
     // Configure wake up pin as input.
     // This will consumes few uA of current.
-    pinMode(wakeUpPin, INPUT);   
+    pinMode(wakeUpPin, INPUT);
+
+    // ATmega328P, ATmega168
+    LowPower.setup(ADC_SWITCH, TIMER2_SWITCH, TIMER1_SWITCH, TIMER0_SWITCH,
+                  SPI_SWITCH, USART0_SWITCH, TWI_SWITCH);
+
+    // ATmega32U4
+    //LowPower.setup(ADC_SWITCH, TIMER4_SWITCH, TIMER3_SWITCH, TIMER1_SWITCH,
+    //		  TIMER0_SWITCH, SPI_SWITCH, USART1_SWITCH, TWI_SWITCH, USB_SWITCH);
+
+    // ATmega2560
+    //LowPower.setup(ADC_SWITCH, TIMER5_SWITCH, TIMER4_SWITCH, TIMER3_SWITCH,
+    //		  TIMER2_SWITCH, TIMER1_SWITCH, TIMER0_SWITCH, SPI_SWITCH, USART3_SWITCH,
+    //		  USART2_SWITCH, USART1_SWITCH, USART0_SWITCH, TWI_SWITCH);
+
+    // ATmega256RFR2
+    //LowPower.setup(ADC_SWITCH, TIMER5_SWITCH, TIMER4_SWITCH, TIMER3_SWITCH,
+    //      TIMER2_SWITCH, TIMER1_SWITCH, TIMER0_SWITCH, SPI_SWITCH,
+    //      USART1_SWITCH, USART0_SWITCH, TWI_SWITCH);
+
 }
 
-void loop() 
+void loop()
 {
     // Allow wake up pin to trigger interrupt on low.
     attachInterrupt(0, wakeUp, LOW);
-    
+
     // Enter power down state with ADC and BOD module disabled.
     // Wake up when wake up pin is low.
-    LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF); 
-    
+    LowPower.powerDown(SLEEP_FOREVER);
+
     // Disable external pin interrupt on wake up pin.
-    detachInterrupt(0); 
-    
+    detachInterrupt(0);
+
     // Do something here
     // Example: Read sensor, data logging, data transmission.
 }
