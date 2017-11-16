2d7323998efca50128c6ceddea2201335ca3e484
rufuswilson <marsou75@gmail.com>
2017-11-16 18:07:21 +0100

    Updated to v1.7

--- Examples/idleWakePeriodic/idleWakePeriodic.ino
+++ Examples/idleWakePeriodic/idleWakePeriodic.ino
@@ -3,34 +3,36 @@
 
 void setup()
 {
-  // No setup is required for this library
+  // Setup the peripherals to be turned off while in sleep
+  // Each microcontroller comes with different number of peripherals
+
+  // ATmega328P, ATmega168
+  LowPower.setup(ADC_SWITCH, TIMER2_SWITCH, TIMER1_SWITCH, TIMER0_SWITCH,
+                SPI_SWITCH, USART0_SWITCH, TWI_SWITCH);
+
+  // ATmega32U4
+  //LowPower.setup(ADC_SWITCH, TIMER4_SWITCH, TIMER3_SWITCH, TIMER1_SWITCH,
+  //		  TIMER0_SWITCH, SPI_SWITCH, USART1_SWITCH, TWI_SWITCH, USB_SWITCH);
+
+  // ATmega2560
+  //LowPower.setup(ADC_SWITCH, TIMER5_SWITCH, TIMER4_SWITCH, TIMER3_SWITCH,
+  //		  TIMER2_SWITCH, TIMER1_SWITCH, TIMER0_SWITCH, SPI_SWITCH, USART3_SWITCH,
+  //		  USART2_SWITCH, USART1_SWITCH, USART0_SWITCH, TWI_SWITCH);
+
+  // ATmega256RFR2
+  //LowPower.setup(ADC_SWITCH, TIMER5_SWITCH, TIMER4_SWITCH, TIMER3_SWITCH,
+  //      TIMER2_SWITCH, TIMER1_SWITCH, TIMER0_SWITCH, SPI_SWITCH,
+  //      USART1_SWITCH, USART0_SWITCH, TWI_SWITCH);
+
 }
 
-void loop() 
+void loop()
 {
   // Enter idle state for 8 s with the rest of peripherals turned off
   // Each microcontroller comes with different number of peripherals
-  // Comment off line of code where necessary
 
-  // ATmega328P, ATmega168
-  LowPower.idle(SLEEP_8S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, 
-                SPI_OFF, USART0_OFF, TWI_OFF);
-
-  // ATmega32U4
-  //LowPower.idle(SLEEP_8S, ADC_OFF, TIMER4_OFF, TIMER3_OFF, TIMER1_OFF, 
-  //		  TIMER0_OFF, SPI_OFF, USART1_OFF, TWI_OFF, USB_OFF);
-
-  // ATmega2560
-  //LowPower.idle(SLEEP_8S, ADC_OFF, TIMER5_OFF, TIMER4_OFF, TIMER3_OFF, 
-  //		  TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART3_OFF, 
-  //		  USART2_OFF, USART1_OFF, USART0_OFF, TWI_OFF);
-
-  // ATmega256RFR2
-  //LowPower.idle(SLEEP_8S, ADC_OFF, TIMER5_OFF, TIMER4_OFF, TIMER3_OFF, 
-  //      TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF,
-  //      USART1_OFF, USART0_OFF, TWI_OFF);
+  LowPower.idle(SLEEP_8S);
 
   // Do something here
   // Example: Read sensor, data logging, data transmission.
 }
-
