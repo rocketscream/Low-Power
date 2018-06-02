09f7e76414e27dd45952534caaeb7d119c254786
Alexander Reinert <alex@areinert.de>
2018-06-02 21:34:23 +0200

    Added support for ATmega644P and ATmega1284P

--- LowPower.h
+++ LowPower.h
@@ -124,6 +124,10 @@ class LowPowerClass
 				void	idle(period_t period, adc_t adc, timer2_t timer2, 
 						     timer1_t timer1, timer0_t timer0, spi_t spi,
 					         usart0_t usart0, twi_t twi);
+			#elif defined __AVR_ATmega644P__ || defined (__AVR_ATmega1284P__)
+				void	idle(period_t period, adc_t adc, timer2_t timer2,
+							 timer1_t timer1, timer0_t timer0, spi_t spi,
+							 usart1_t usart1, usart0_t usart0, twi_t twi);
 			#elif defined __AVR_ATmega2560__
 				void	idle(period_t period, adc_t adc, timer5_t timer5, 
 							 timer4_t timer4, timer3_t timer3, timer2_t timer2,
