51dd2204dc2215c51346facabe321d72ad461e21
Maor Kleinberger <kmaork@gmail.com>
2021-08-24 17:22:02 +0300

    Use `digitalPinToInterrupt(wakeUpPin)` instead of hardcoded interrupt no.

--- Examples/powerDownWakeExternalInterrupt/powerDownWakeExternalInterrupt.ino
+++ Examples/powerDownWakeExternalInterrupt/powerDownWakeExternalInterrupt.ino
@@ -19,14 +19,14 @@ void setup()
 void loop() 
 {
     // Allow wake up pin to trigger interrupt on low.
-    attachInterrupt(0, wakeUp, LOW);
+    attachInterrupt(digitalPinToInterrupt(wakeUpPin), wakeUp, LOW);
     
     // Enter power down state with ADC and BOD module disabled.
     // Wake up when wake up pin is low.
     LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF); 
     
     // Disable external pin interrupt on wake up pin.
-    detachInterrupt(0); 
+    detachInterrupt(digitalPinToInterrupt(wakeUpPin)); 
     
     // Do something here
     // Example: Read sensor, data logging, data transmission.
